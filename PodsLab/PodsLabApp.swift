e244c499835af4394c540ff9e32c3666f8b32990
simicity <simi40nm@gmail.com>
2024-05-18 12:37:47 -0700

    Initial Commit

---
+++ PodsLab/PodsLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodsLabApp.swift
+//  PodsLab
+//
+//  Created by Miho Shimizu on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct PodsLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
